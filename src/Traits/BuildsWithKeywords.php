c576533d1d50bf7fe61a5005a4cc461747156b79
Justin Scott <justin@hitpath.com>
2020-07-23 15:47:18 -0500

    Expands the documentation for buildswithkeywords.

--- src/Traits/BuildsWithKeywords.php
+++ src/Traits/BuildsWithKeywords.php
@@ -16,8 +16,12 @@ trait BuildsWithKeywords {
      * Factory method for creating objects with properties from this.
      *
      * @param string $klass - The class to build
+     * @param array $override - Additional arguments for the target class constructor.
+     * @param array $alias - Renamings of properties to
+     *
+     * @return object of the type given as the first argument
      */
-    public function build(string $klass, array $override = [], array $alias = []) {
+    public function build(string $klass, array $override = [], array $alias = []) : object {
         $maker = KeywordMaker::forClass($klass);
 
         $refClass = new ReflectionClass($this);
